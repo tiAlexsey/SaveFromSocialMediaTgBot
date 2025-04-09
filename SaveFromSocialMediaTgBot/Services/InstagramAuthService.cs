8abc2c7bc05d07f968d60854f22f78d3fbad17cb
tialexsey <tiAlexsey@gmail.com>
2025-04-09 16:53:33 +0200

    trying fix inst

--- SaveFromSocialMediaTgBot/Services/InstagramAuthService.cs
+++ SaveFromSocialMediaTgBot/Services/InstagramAuthService.cs
@@ -1,12 +1,14 @@
 using PuppeteerSharp;
+using PuppeteerSharp.Input;
 
 namespace SaveFromSocialMediaTgBot.Services;
 
 public static class InstagramAuthService
 {
+    private static readonly Random _random = new();
     private static readonly LaunchOptions _launchOptions = new()
     {
-        Headless = true,
+        Headless = false,
         ExecutablePath = "/usr/bin/chromium",
         Args = ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"]
     };
@@ -25,14 +27,16 @@ public static class InstagramAuthService
         await page.WaitForSelectorAsync("input[name='username']");
         await page.WaitForSelectorAsync("input[name='password']");
 
+        await Task.Delay(_random.Next(800, 1000));
         // Ввод данных
-        await page.TypeAsync("input[name='username']", login);
-        await page.TypeAsync("input[name='password']", password);
+        await page.TypeAsync("input[name='username']", login, new TypeOptions { Delay = 150 });
+        await Task.Delay(_random.Next(500, 1000));
+        await page.TypeAsync("input[name='password']", password, new TypeOptions { Delay = 150 });
 
         // Нажатие кнопки входа
         await page.ClickAsync("button[type='submit']");
         // Ждем авторизацию
-        await Task.Delay(10000);
+        await page.WaitForNavigationAsync(new NavigationOptions { WaitUntil = [WaitUntilNavigation.Networkidle0] });
 
         using var page2 = await browser.NewPageAsync();
         await page2.GoToAsync("https://www.instagram.com/", WaitUntilNavigation.Networkidle0);
