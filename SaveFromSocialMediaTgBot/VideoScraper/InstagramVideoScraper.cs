273784f78a257c7a36e7b0cb9809fc447739f31a
tialexsey <tiAlexsey@gmail.com>
2025-05-09 13:53:58 +0200

    Fixed inst

--- SaveFromSocialMediaTgBot/VideoScraper/InstagramVideoScraper.cs
+++ SaveFromSocialMediaTgBot/VideoScraper/InstagramVideoScraper.cs
@@ -1,17 +1,18 @@
 using System.Text.RegularExpressions;
 using PuppeteerSharp;
+using PuppeteerSharp.Input;
 using SaveFromSocialMediaTgBot.Abstract.Interface;
 using SaveFromSocialMediaTgBot.Data.Const;
-using SaveFromSocialMediaTgBot.Extensions.Helper;
 
 namespace SaveFromSocialMediaTgBot.VideoScraper;
 
 public class InstagramVideoScraper(IConfiguration configuration, HttpClient client) : IVideoScraper
 {
+    private readonly Random random = new();
     private readonly Regex pattern = new(Pattern.INSTAGRAM, RegexOptions.Compiled);
+    private string sessionId = configuration[Env.INST_COOKIE_SESSION_ID] ?? "";
     private readonly string login = configuration[Env.INST_LOGIN] ?? "";
     private readonly string password = configuration[Env.INST_PASSWORD] ?? "";
-    private string sessionId = configuration[Env.INST_COOKIE_SESSION_ID] ?? "";
 
     private readonly LaunchOptions launchOptions = new()
     {
@@ -19,6 +20,8 @@ public class InstagramVideoScraper(IConfiguration configuration, HttpClient clie
         ExecutablePath = "/usr/bin/chromium",
         Args = ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"]
     };
+
+    private static CookieParam[] Cookies { get; set; } = [];
 
     public bool CanHandle(string url)
         => url.Contains("instagram.com", StringComparison.OrdinalIgnoreCase);
@@ -35,11 +38,10 @@ public class InstagramVideoScraper(IConfiguration configuration, HttpClient clie
         await using var browser = await Puppeteer.LaunchAsync(launchOptions);
         // Открываем новую страницу в браузере
         await using var page = await browser.NewPageAsync();
-        // Ставим  куки
+        // Ставим куки
         await SetCookies(page);
 
         var tryCount = 0;
-
         try
         {
             Match? match = null;
@@ -47,18 +49,18 @@ public class InstagramVideoScraper(IConfiguration configuration, HttpClient clie
             {
                 // Переходим по URL
                 await page.GoToAsync(pageUrl, WaitUntilNavigation.Networkidle0);
+                await Task.Delay(random.Next(500, 1000));
                 // Выкачиваем html страницу
                 var content = await page.GetContentAsync();
 
                 match = pattern.Match(content);
                 if (!match.Success)
                 {
-                    await page.SetCookieAsync(await InstagramHelper.AuthorizationAsync(login, password));
+                    await page.SetCookieAsync(await AuthorizationAsync());
+                    continue;
                 }
-                else
-                {
-                    tryCount++;
-                }
+
+                tryCount++;
             }
 
             // Закрываем браузер
@@ -83,18 +85,47 @@ public class InstagramVideoScraper(IConfiguration configuration, HttpClient clie
 
     private async Task SetCookies(IPage page)
     {
-        if (string.IsNullOrWhiteSpace(sessionId)) return;
-        InstagramHelper.Cookies =
-        [
-            new CookieParam
-            {
-                Name = "sessionid",
-                Value = sessionId,
-                Domain = ".instagram.com"
-            }
-        ];
-        sessionId = string.Empty;
+        if (!string.IsNullOrWhiteSpace(sessionId))
+        {
+            Cookies =
+            [
+                new CookieParam
+                {
+                    Name = "sessionid",
+                    Value = sessionId,
+                    Domain = ".instagram.com"
+                }
+            ];
+            sessionId = string.Empty;
+        }
 
-        await page.SetCookieAsync(InstagramHelper.Cookies);
+        await page.SetCookieAsync(Cookies);
+    }
+
+    private async Task<CookieParam[]> AuthorizationAsync()
+    {
+        await using var browser = await Puppeteer.LaunchAsync(launchOptions);
+        await using var page = await browser.NewPageAsync();
+
+        // Переход на страницу входа Instagram
+        await page.GoToAsync("https://www.instagram.com/accounts/login/");
+
+        // Ожидание появления полей ввода
+        await page.WaitForSelectorAsync("input[name='username']");
+        await page.WaitForSelectorAsync("input[name='password']");
+
+        await Task.Delay(random.Next(800, 1000));
+        // Ввод данных
+        await page.TypeAsync("input[name='username']", login, new TypeOptions { Delay = 150 });
+        await Task.Delay(random.Next(500, 1000));
+        await page.TypeAsync("input[name='password']", password, new TypeOptions { Delay = 150 });
+
+        // Нажатие кнопки входа
+        await page.ClickAsync("button[type='submit']");
+        // Ждем авторизацию
+        await page.WaitForNavigationAsync(new NavigationOptions { WaitUntil = [WaitUntilNavigation.Networkidle0] });
+        await Task.Delay(random.Next(1000, 3000));
+        Cookies = await page.GetCookiesAsync();
+        return Cookies;
     }
 }
